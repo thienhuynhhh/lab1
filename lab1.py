84204e0793c0336e7bd70963523702ea75085b47
Ethan <97563598+thienhuynhhh@users.noreply.github.com>
2023-09-18 18:00:36 -0600

    Step 9

--- lab1.py
+++ lab1.py
@@ -1,4 +1,7 @@
 import requests
-#print(requests.__version__)
-result=requests.get("http://www.google.com")
+#print(requests.__version__)                      
+#result=requests.get("http://www.google.com")
+#print(result.text)
+
+result = requests.get("https://raw.githubusercontent.com/thienhuynhhh/lab1/main/lab1.py")
 print(result.text)
